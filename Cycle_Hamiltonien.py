6e0223d5b8f4d91a0ccdc31fed8f174dbe7aed69
Gaboto74 <gabin.mondongou@gmail.com>
2023-04-04 10:52:26 +0200

    Cycle Hamiltonien

---
+++ Cycle_Hamiltonien.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+************************
+@author : Gabin
+@contact : gabin.mondongou@auxlazaristeslasalle.fr
+created on : Tue Apr 04 10:46:21 2023
+last mod : 2023
+title : notitle
+goals : brief description
+to do : what should be done next time
+************************* """
+
+""" ********************
+    **** Imports ****
+    ******************** """
+# no data
+
+""" ********************
+    **** Classes ****
+    ******************** """
+# no data
+
+""" ********************
+    **** Functions ****
+    ******************** """
+def condition_Dirac(matrice):
+    taille_matrice = len(matrice)
+    for i in range(taille_matrice):
+        
+
+   
+""" ********************
+    **** Global Var ****
+    ******************** """
+# no data
+
+""" **************
+    **** Main ****
+    ************** """
+# no data
